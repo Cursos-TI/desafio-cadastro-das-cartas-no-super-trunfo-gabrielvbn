ee774f577d216422c8640a6deaee730b91c7a693
gabrielvbn <gabriellucena_98@hotmail.com>
2025-03-24 20:02:15 +0000

    Introdução da Função fgets

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,13 @@
 int main() {
  //Variaveis carta 01
 
-    char estado1[20] , cidade1[20] , codigoCarta1 [5];
+    char estado1[50] , cidade1[50] , codigoCarta1 [5];
     int populacao1 , pontosTuristicos1;
     float pib1 , areak2_1;
 
 //Variaveis da carta 02
 
-  char estado2[20] , cidade2[20] , codigoCarta2 [5];
+  char estado2[50] , cidade2[50] , codigoCarta2 [5];
   int populacao2 , pontosTuristicos2;
   float pib2 , areak2_2;
     
@@ -17,59 +17,60 @@ int main() {
 
   printf ("Agora vamos criar a primeira carta do jogo\n");
   printf ("Digite o nome do Estado: ");
-  scanf ("%s" , estado1);
+  fgets (estado1 , 50 , stdin);
   printf ("Digite o nome da Cidade: ");
-  scanf ("%s" , cidade1);
-  printf ("Digite um codigo para a carta(ex: A01,A02): ");
-  scanf ("%s" , codigoCarta1);
+  fgets (cidade1 , 50 , stdin);
+  printf ("Digite um codigo para a carta(Use Letras a A a H , numeros de 1 a 4 , ex A01): ");
+  scanf (" %s" , codigoCarta1);
   printf ("Qual a população desta cidade: ");
-  scanf ("%d" , &populacao1);
+  scanf (" %d" , &populacao1);
   printf ("Qual a area total da cidade em km²: ");
-  scanf ("%f" , &areak2_1);
+  scanf (" %f" , &areak2_1);
   printf ("Qual o PIB desta cidade: ");
-  scanf ("%f" , &pib1);
+  scanf (" %f" , &pib1);
   printf ("Quantos pontos turisticos esta cidade tem: ");
-  scanf ("%d" , &pontosTuristicos1);
+  scanf (" %d" , &pontosTuristicos1);
 
 //Interação com o usuário para incersão de dados para criação da carta 02
 
   printf ("Agora vamos criar a carta 02 do jogo\n");
   printf ("Digite o nome do Estado: ");
-  scanf ("%s" , estado2);
+  getchar ();
+  fgets (estado2 , 50 , stdin);
   printf ("Digite o nome da Cidade: ");
-  scanf ("%s" , cidade2);
-  printf ("Digite um codigo para a carta(ex: A01,A02): ");
-  scanf ("%s" , codigoCarta2);
+  fgets (cidade2 , 50 , stdin);
+  printf ("Digite um codigo para a carta(Use Letras a A a H , numeros de 1 a 4 , ex A01): ");
+  scanf (" %s" , codigoCarta2);
   printf ("Qual a população desta cidade:");
-  scanf ("%d" , &populacao2);
+  scanf (" %d" , &populacao2);
   printf ("Qual a area total da cidade em km²: ");
-  scanf ("%f" , &areak2_2);
+  scanf (" %f" , &areak2_2);
   printf ("Qual o PIB desta cidade: ");
-  scanf ("%f" , &pib2);
+  scanf (" %f" , &pib2);
   printf ("Quantos pontos turisticos esta cidade tem: ");
-  scanf ("%d" , &pontosTuristicos2);
+  scanf (" %d" , &pontosTuristicos2);
 
 //Exibição da carta 01 criadas na tela do usuario
 
  printf ("Carta 01\n");
- printf ("Estado: " "%s/n" , estado1);
- printf ("Cidade: " "%s\n" , cidade1);
- printf ("Código da carta: " "%s\n" , codigoCarta1);
- printf ("População: " "%d\n" , populacao1);
- printf ("Area em K²: " "%f\n" , areak2_1);
- printf ("PIB: " "%f\n" , pib1);
- printf ("Quantidade de pontos turisticos: " "%d\n" , pontosTuristicos1);
+ printf ("Estado: %s" , estado1);
+ printf ("Cidade: %s" , cidade1);
+ printf ("Código da carta: %s\n" , codigoCarta1);
+ printf ("População: %d\n" , populacao1);
+ printf ("Area em K²: %f\n" , areak2_1);
+ printf ("PIB: %f\n" , pib1);
+ printf ("Quantidade de pontos turisticos: %d\n" , pontosTuristicos1);
 
  //Exibição da carta 02 criadas na tela do usuario
 
  printf ("Carta 02\n");
- printf ("Estado: " "%s/n" , estado2);
- printf ("Cidade: " "%s\n" , cidade2);
- printf ("Código da carta: " "%s\n" , codigoCarta2);
- printf ("População: " "%d\n" , populacao2);
- printf ("Area em K²: " "%f\n" , areak2_2);
- printf ("PIB: " "%f\n" , pib2);
- printf ("Quantidade de pontos turisticos: " "%d\n" , pontosTuristicos2);
+ printf ("Estado: %s" , estado2);
+ printf ("Cidade: %s" , cidade2);
+ printf ("Código da carta: %s\n" , codigoCarta2);
+ printf ("População: %d\n" , populacao2);
+ printf ("Area em K²: %f\n" , areak2_2);
+ printf ("PIB: %f\n" , pib2);
+ printf ("Quantidade de pontos turisticos: %d\n" , pontosTuristicos2);
 
 
     return 0;
